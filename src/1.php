34437050ebe1dd7dfd7d94cfad08be123da29838
niedbalaknoebel137 <niedbalaknoebel137@gmail.com>
2025-03-06 13:01:41 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,29 @@
+
+<?php
+
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+    return mt_rand(1, 10);
+}
+
+// Function to calculate the sum of two numbers
+function sum($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Function to calculate the difference of two numbers
+function diff($num1, $num2) {
+    return $num1 - $num2;
+}
+
+// Function to calculate the product of two numbers
+function prod($num1, $num2) {
+    return $num1 * $num2;
+}
+
+// Function to calculate the quotient of two numbers
+function quot($num1, $num2) {
+    return $num1 / $num2;
+}
+
+?>
