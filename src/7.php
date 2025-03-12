519a48c28e23c8b6e2cce44e6d6047d0bd0f7787
niedbalaknoebel137 <niedbalaknoebel137@gmail.com>
2025-03-12 01:13:45 +0000

    Add 7.php

---
+++ src/7.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$math_homework = array(
+	array('problem' => 'Find the value of x in the equation 2x + 5 = 11', 'solution' => 'x = 3'),
+	array('problem' => 'Simplify the expression (3x - 2) / (x + 1)', 'solution' => '(3x - 2) / (x + 1) = 3 - 2/x - 1'),
+	array('problem' => 'Solve for x in the equation 2x + 6 = 13', 'solution' => 'x = 5'),
+);
+
+?>
