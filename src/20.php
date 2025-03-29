982d13228ffd19265638d3ad4c8d4cf9af1b8f63
niedbalaknoebel137 <niedbalaknoebel137@gmail.com>
2025-03-29 20:07:00 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables
+$username = "admin";
+$password = "password";
+
+// Check if username and password are correct
+if ($username == "admin" && $password == "password") {
+    // User is authenticated successfully
+} else {
+    // Authentication failed, display an error message
+}
+?>
